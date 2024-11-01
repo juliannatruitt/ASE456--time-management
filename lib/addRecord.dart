3ce35e7e88c1a7c0e3d6854603aab7fa0105da24
juliannatruitt <truittj3@nku.edu>
2024-10-31 21:16:08 -0400

    making updates to version 1

--- lib/addRecord.dart
+++ lib/addRecord.dart
@@ -16,6 +16,12 @@ class AddRecord extends StatefulWidget {
 
 class _AddRecordState extends State<AddRecord> {
   late String _selectedValue;
+  late String _timeInHoursFrom='12';
+  late String _timeInMinutesFrom='00';
+  late String _amOrPmFrom='PM';
+  late String _timeInHoursTo='1';
+  late String _timeInMinutesTo='00';
+  late String _amOrPmTo='PM';
   late var currentTime;
   late Future<Set<dynamic>> allTags;
   List<dynamic> tagsToList =[];
@@ -29,6 +35,7 @@ class _AddRecordState extends State<AddRecord> {
     super.initState();
     allTags = getTags();
   }
+
 
   @override
   Widget build(BuildContext context){
@@ -57,22 +64,110 @@ class _AddRecordState extends State<AddRecord> {
                   ),
                   Padding(
                       padding: const EdgeInsets.all(5.0),
-                      child: TextField(
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(),   // bordered outline
-                          labelText: 'FROM',
+                      child: Row(children: [
+                        DropdownButton(
+                        value: _timeInHoursFrom,
+                        items: ['1','2','3','4','5','6','7','8','9','10','11','12'].map(
+                                (value){
+                              return DropdownMenuItem(
+                                value: value,
+                                child: Text(value),
+                              );
+                            }
+                        ).toList(),
+                        onChanged: (value) {
+                          setState(() {
+                            _timeInHoursFrom = value as String;
+                          });
+                        },
+                      ),
+                        DropdownButton(
+                          value: _timeInMinutesFrom,
+                          items: ['00','05','10','15','20','25','30','35','40','45','50','55'].map(
+                                  (value){
+                                return DropdownMenuItem(
+                                  value: value,
+                                  child: Text(value),
+                                );
+                              }
+                          ).toList(),
+                          onChanged: (value) {
+                            setState(() {
+                              _timeInMinutesFrom = value as String;
+                            });
+                          },
                         ),
-                        onChanged: (text) {_timeFrom = text;}
+                        DropdownButton(
+                          value: _amOrPmFrom,
+                          items: ['AM','PM'].map(
+                                  (value){
+                                return DropdownMenuItem(
+                                  value: value,
+                                  child: Text(value),
+                                );
+                              }
+                          ).toList(),
+                          onChanged: (value) {
+                            setState(() {
+                              _amOrPmFrom = value as String;
+                            });
+                          },
+                        ),
+                      ],
                       ),
                   ),
                   Padding(
                       padding: const EdgeInsets.all(5.0),
-                      child: TextField(
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(),   // bordered outline
-                          labelText: 'TO',
+                      child: Row(children: [
+                        DropdownButton(
+                          value: _timeInHoursTo,
+                          items: ['1','2','3','4','5','6','7','8','9','10','11','12'].map(
+                                  (value){
+                                return DropdownMenuItem(
+                                  value: value,
+                                  child: Text(value),
+                                );
+                              }
+                          ).toList(),
+                          onChanged: (value) {
+                            setState(() {
+                              _timeInHoursTo = value as String;
+                            });
+                          },
                         ),
-                          onChanged: (text) {_timeTo=text;}
+                        DropdownButton(
+                          value: _timeInMinutesTo,
+                          items: ['00','05','10','15','20','25','30','35','40','45','50','55'].map(
+                                  (value){
+                                return DropdownMenuItem(
+                                  value: value,
+                                  child: Text(value),
+                                );
+                              }
+                          ).toList(),
+                          onChanged: (value) {
+                            setState(() {
+                              _timeInMinutesTo = value as String;
+                            });
+                          },
+                        ),
+                        DropdownButton(
+                          value: _amOrPmTo,
+                          items: ['AM','PM'].map(
+                                  (value){
+                                return DropdownMenuItem(
+                                  value: value,
+                                  child: Text(value),
+                                );
+                              }
+                          ).toList(),
+                          onChanged: (value) {
+                            setState(() {
+                              _amOrPmTo = value as String;
+                            });
+                          },
+                        ),
+                      ],
                       ),
                   ),
                   Padding(
@@ -173,7 +268,9 @@ class _AddRecordState extends State<AddRecord> {
         //time -> valid (with or without am/pm)
         //tag -> one word
         onPressed:() async{
-          if (currentTime != null && _timeFrom != null && _timeTo != null && _description != null && _selectedValue != null){
+          if (currentTime != null && _description != null && _selectedValue != null){
+            _timeFrom = "$_timeInHoursFrom:$_timeInMinutesFrom$_amOrPmFrom";
+            _timeTo = "$_timeInHoursTo:$_timeInMinutesTo$_amOrPmTo";
             await addRecord(currentTime, _timeFrom, _timeTo, _description, _selectedValue);
             Navigator.push(
                 context,
