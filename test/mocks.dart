ad5fa661e92dc572d78f1ce04d067d6867e800b1
juliannatruitt <truittj3@nku.edu>
2024-11-28 23:15:49 -0500

    deleting old documents

--- test/mocks.dart
+++
@@ -1,6 +0,0 @@
-import 'package:mockito/annotations.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-
-@GenerateMocks([FirebaseFirestore, CollectionReference, QuerySnapshot, QueryDocumentSnapshot])
-void main() {}
